defab00efb4d8422c7a5a5e10cc66a065a635ff6
jlml <jesus.lopez@aytoarga.es>
2017-12-21 14:28:31 +0100

    cambiado puerto y modelo

--- displacy/app.py
+++ displacy/app.py
@@ -1,11 +1,21 @@
 from displacy_service.server import APP, get_model
 
 # Pre-load English model only, to save memory
-get_model('en_core_web_sm')
+model='es_core_news_md'
+print("model:" + str(model))
+print("loading....")
+
+# get_model('en_core_web_sm')
 # get_model('de')
+get_model(model)
+print("[ok]")
 
 
 if __name__ == '__main__':
     from wsgiref import simple_server
-    httpd = simple_server.make_server('0.0.0.0', 8000, APP)
+    ip='0.0.0.0'
+    port=7999
+    httpd = simple_server.make_server(ip, port, APP)
+    print("ip:" + ip)
+    print("port:" + str(port))
     httpd.serve_forever()
--- displacy/displacy_service/parse.py
+++ displacy/displacy_service/parse.py
@@ -53,7 +53,7 @@ class Parse(object):
 class Entities(object):
     def __init__(self, nlp, text):
         self.doc = nlp(text)
-     
+
     def to_json(self):
-        return [{'start': ent.start_char, 'end': ent.end_char, 'type': ent.label_}
+        return [{'text': ent.text , 'start': ent.start_char, 'end': ent.end_char, 'type': ent.label_}
                 for ent in self.doc.ents]
